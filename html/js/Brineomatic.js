1e48be30b5d516bb31a16df22a434b9671e2e6c6
Zach Hoeken <hoeken@gmail.com>
2026-04-18 08:58:31 -1000

    fixed ui

--- html/js/Brineomatic.js
+++ html/js/Brineomatic.js
@@ -2732,12 +2732,14 @@
               <div class="invalid-feedback"></div>
             </div>
           </div>
-          <div class="col-12 mt-1">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="high_pressure_stepper_inverted">
-              <label class="form-check-label" for="high_pressure_stepper_inverted">Invert Stepper Direction</label>
-            </div>
-          </div>
+        </div>
+
+        <div class="form-check form-switch mb-3">
+            <input class="form-check-input" type="checkbox" id="high_pressure_stepper_inverted">
+            <label class="form-check-label" for="high_pressure_stepper_inverted">
+                Is Stepper Motor Direction Inverted?
+            </label>
+            <div class="invalid-feedback"></div>
         </div>
       </div>
 
